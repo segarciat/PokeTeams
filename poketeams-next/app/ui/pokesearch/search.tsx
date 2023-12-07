0620f3732222ea6494a63925c30964f1cc0b5a5a
Sergio Garcia <segarciat314@gmail.com>
2023-12-07 15:25:13 -0500

    Add submit button

--- poketeams-next/app/ui/pokesearch/search.tsx
+++ poketeams-next/app/ui/pokesearch/search.tsx
@@ -7,8 +7,6 @@ export interface SearchProps {
   placeholder: string
 }
 
-const leftInputIconClass = 'absolute left-3 top-4 text-gray-400'
-
 export default function Search ({ placeholder }: SearchProps): ReactElement {
   const searchParams = useSearchParams()
   const pathname = usePathname()
@@ -38,29 +36,33 @@ export default function Search ({ placeholder }: SearchProps): ReactElement {
 
   return (
     <section aria-label="search options" className='my-4'>
-      <form role='search' aria-label='pokesearch form' className='relative flex flex-1' onSubmit={handleSubmit}>
+      <form role='search' aria-label='pokesearch form' className='relative flex flex-1 flex-col gap-2' onSubmit={handleSubmit}>
         <div className='relative w-full'>
           <input
             id='query' // Matches value in label htmlFor
             name='query' // Used to access form submission element.
             className='peer placeholder:text-transparent bg-white rounded-2xl border outline-gray-300
-        border-gray-300 w-full py-3 pl-9 text-base placeholder:text-gray-500'
+        border-gray-300 w-full py-3 text-base pl-8 placeholder:text-gray-500'
             placeholder={placeholder}
             value={query}
             onChange={handleChange}
           />
-          <label htmlFor="query" className='absolute left-9 top-[1px] text-xs text-gray-400
+          <label htmlFor="query" className='absolute left-8 top-[1px] text-xs text-gray-400
         peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-focus:top-[1px] =
         peer-focus:text-xs transition-all'>
             {placeholder}
           </label>
         </div>
-        {query.length === 0
-          ? <MagnifyingGlassIcon height={18} width={18} className={leftInputIconClass} />
-          : <button aria-label='clear input' type='button' className={leftInputIconClass} onClick={handleClearInput}>
-              <XMarkIcon height={18} width={18} />
-            </button>
+        {query.length !== 0 && (
+          <button aria-label='clear input' type='button' className='absolute left-3 top-4 text-gray-400' onClick={handleClearInput}>
+            <XMarkIcon height={18} width={18} />
+          </button>
+        )
         }
+        <button type='submit' className='text-white bg-primary p-2 rounded-2xl flex flex-row items-center justify-center gap-2'>
+          <MagnifyingGlassIcon className='inline-block' height={18} width={18} />
+          Search
+        </button>
       </form>
     </section>
   )
