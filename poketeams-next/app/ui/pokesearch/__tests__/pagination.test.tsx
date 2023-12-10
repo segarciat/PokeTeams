00d9fc4f80c09233bd7f8754f0b045685dd64c91
Sergio Garcia <segarciat314@gmail.com>
2023-12-10 17:15:43 -0500

    Update pagination and tests

--- poketeams-next/app/ui/pokesearch/__tests__/pagination.test.tsx
+++ poketeams-next/app/ui/pokesearch/__tests__/pagination.test.tsx
@@ -1,33 +1,63 @@
 import { render, screen, within } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 import Pagination from '../pagination'
-
-const { mockReplace, mockUsePathname, mockUseSearchParams } = vi.hoisted(() => ({
-  mockUseSearchParams: vi.fn(),
-  mockUsePathname: vi.fn().mockReturnValue('/path'),
-  mockReplace: vi.fn()
-}))
-
-vi.mock('next/navigation', () => ({
-  useSearchParams: mockUseSearchParams,
-  usePathname: mockUsePathname,
-  useRouter: vi.fn().mockReturnValue({ replace: mockReplace })
-}))
+import userEvent from '@testing-library/user-event'
+import { type PokeSearchParamAction } from '@/app/lib/definitions'
 
 describe('pagination', () => {
-  it('should display next link if the current is not the last page', () => {
+  it('should display next and last link if the current is not the last page', () => {
     const handlePageClick = vi.fn()
     render(<Pagination page={1} totalPages={7} onPageClick={handlePageClick}/>)
-    const pagination = screen.getByRole('navigation', { name: /pagination/i })
+    const pagination = screen.getByRole('list', { name: /pagination/i })
     expect(pagination).toBeVisible()
-    expect(within(pagination).getByRole('button', { name: /next/i })).toBeVisible()
+    expect(within(pagination).getByText(/1/i))
+    expect(within(pagination).getByLabelText(/next/i)).toBeVisible()
+    expect(within(pagination).getByLabelText(/last/i)).toBeVisible()
   })
 
-  it('should display previous link if current is not first page', () => {
+  it('should display previous and first link if current is not first page', () => {
     const handlePageClick = vi.fn()
     render(<Pagination page={7} totalPages={7} onPageClick={handlePageClick}/>)
-    const pagination = screen.getByRole('navigation', { name: /pagination/i })
+    const pagination = screen.getByRole('list', { name: /pagination/i })
     expect(pagination).toBeVisible()
-    expect(within(pagination).getByRole('button', { name: /previous/i })).toBeVisible()
+    expect(within(pagination).getByText(/7/i)).toBeVisible()
+    expect(within(pagination).getByLabelText(/previous/i)).toBeVisible()
+    expect(within(pagination).getByLabelText(/first/i)).toBeVisible()
+  })
+
+  it('should call handler with correct page number when page button is clicked', async () => {
+    const handlePageClick = vi.fn()
+    const currentPage = 5
+    const lastPage = 10
+    render(<Pagination page={currentPage} totalPages={lastPage} onPageClick={handlePageClick} />)
+
+    await userEvent.click(screen.getByText(/5/i)) // should not register event, it's current page.
+    await userEvent.click(screen.getByText(/6/i))
+    await userEvent.click(screen.getByLabelText(/previous/i))
+    await userEvent.click(screen.getByLabelText(/next/i))
+    await userEvent.click(screen.getByLabelText(/first/i))
+    await userEvent.click(screen.getByLabelText(/last/i))
+
+    expect(handlePageClick).toHaveBeenCalledTimes(5)
+    expect(handlePageClick.mock.calls[0][0]).toEqual({ action: 'NEW_PAGE', page: 6 } as any as PokeSearchParamAction)
+    expect(handlePageClick.mock.calls[1][0]).toEqual({ action: 'NEW_PAGE', page: currentPage - 1 } as any as PokeSearchParamAction)
+    expect(handlePageClick.mock.calls[2][0]).toEqual({ action: 'NEW_PAGE', page: currentPage + 1 } as any as PokeSearchParamAction)
+    expect(handlePageClick.mock.calls[3][0]).toEqual({ action: 'NEW_PAGE', page: 1 } as any as PokeSearchParamAction)
+    expect(handlePageClick.mock.calls[4][0]).toEqual({ action: 'NEW_PAGE', page: lastPage } as any as PokeSearchParamAction)
+  })
+
+  it('should go to the typed page number when page number form is submitted', async () => {
+    const handlePageClick = vi.fn()
+    const currentPage = 5
+    const lastPage = 10
+    render(<Pagination page={currentPage} totalPages={lastPage} onPageClick={handlePageClick} />)
+
+    expect(screen.getByLabelText(/desired page/i)).toHaveValue(5)
+    await userEvent.type(screen.getByLabelText(/desired page/i), '{backspace}4')
+    expect(screen.getByLabelText(/desired page/i)).toHaveValue(4)
+    await userEvent.click(screen.getByRole('button', { name: /go/i }))
+
+    expect(handlePageClick).toHaveBeenCalledTimes(1)
+    expect(handlePageClick.mock.calls[0][0]).toEqual({ action: 'NEW_PAGE', page: 4 } as any as PokeSearchParamAction)
   })
 })
