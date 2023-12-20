97c2f93ace79fed02d734cdecd2e0a7a0221dadd
Sergio Garcia <segarciat314@gmail.com>
2023-12-20 18:43:32 -0500

    Replace type filters when type button clicked on card

--- app/ui/pokesearch/pokemon/summary.tsx
+++ app/ui/pokesearch/pokemon/summary.tsx
@@ -1,8 +1,9 @@
-import { type ReactElement } from 'react'
+import { useContext, type ReactElement } from 'react'
 import { type Pokemon } from '@/app/lib/definitions'
 import { capitalize } from '@/app/lib/utils'
 import PokeTypeTag from '../../poke-type-tags'
-import { POKE_TYPE_BG_CLASS } from '@/app/lib/constants'
+import { POKE_TYPE_BG_CLASS, type PokeType } from '@/app/lib/constants'
+import TypesParamContext from '@/app/context/poke-types-param'
 
 interface SummaryProps {
   types: Pokemon['types']
@@ -12,6 +13,12 @@ interface SummaryProps {
 const attributeTitleClass = 'text-sm text-gray-300 font-bold'
 
 export default function Summary ({ types, name }: SummaryProps): ReactElement {
+  const { setTypes } = useContext(TypesParamContext)
+
+  function handleTypeClick (type: PokeType): void {
+    setTypes(new Set<PokeType>([type]))
+  }
+
   return (
     <section arial-label="pokemon summary">
       <h3 className={attributeTitleClass}>Name</h3>
@@ -21,7 +28,7 @@ export default function Summary ({ types, name }: SummaryProps): ReactElement {
       <ul className='flex justify-center gap-2 my-2'>
         {types.map((type) => (
           <li key={type.name}>
-            <button className={`px-4 py-2 ${POKE_TYPE_BG_CLASS[type.name]} text-white rounded-full text-sm`}>
+            <button onClick={handleTypeClick.bind(null, type.name)} className={`px-4 py-2 ${POKE_TYPE_BG_CLASS[type.name]} text-white rounded-full text-sm`}>
               <PokeTypeTag pokeType={type.name}/>
             </button>
           </li>
